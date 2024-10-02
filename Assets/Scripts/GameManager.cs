74d20c0f8f6a4bf9df38ba3ff1b70bce0ca83f82
anMois <kew1672@naver.com>
2024-10-02 16:20:22 +0900

    [Feat] UI 최고 점수 기록 갱신
    최고 점수 갱신 시 new record와 함께 점수 갱신

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance { get; private set; }
     public const int SCORE = 100;
+    
+    [SerializeField] int highScore;
 
+    public int HighScore { get { return highScore; } set { highScore = value; } }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/InGameManager.cs
+++ Assets/Scripts/InGameManager.cs
@@ -7,26 +7,51 @@ public class InGameManager : MonoBehaviour
 {
     [Header("UI")]
     [SerializeField] TextMeshProUGUI curScoreText;
-    [SerializeField] TextMeshProUGUI bestScoreText;
+    [SerializeField] TextMeshProUGUI highScoreText;
+    [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] GameObject recordText;
     [SerializeField] Slider feverGauge;
+    [SerializeField] Animator animator;
 
     [Space]
     [SerializeField] int curScore;
+    [SerializeField] int score;
     [SerializeField, Range(0, 0.5f)] float getFeverGauge;
     [SerializeField] bool fever;
 
     private StringBuilder sb = new StringBuilder();
+    private int highScore;
 
     private void Awake()
     {
         Init();
+        score = GameManager.SCORE;
+        highScore = GameManager.Instance.HighScore;
     }
 
+    private void OnDisable()
+    {
+        if (recordText.activeSelf)
+            GameManager.Instance.HighScore = curScore;
+    }
+
+    private void LateUpdate()
+    {
+        TextBuilder(curScore.ToString(), curScoreText);
+        FeverGaugeDown();
+
+        if(highScore < curScore)
+        {
+            recordText.SetActive(true);
+            TextBuilder(curScore.ToString(), highScoreText);
+        }
+    }
     private void Init()
     {
         TextBuilder(curScore.ToString(), curScoreText);
+        TextBuilder(highScore.ToString(), highScoreText);
         recordText.SetActive(false);
+        feverGauge.value = 0;
     }
 
     private void TextBuilder(string msg, TextMeshProUGUI text)
@@ -36,30 +61,22 @@ public class InGameManager : MonoBehaviour
         text.SetText(sb);
     }
 
-    private void LateUpdate()
-    {
-        TextBuilder(curScore.ToString(), curScoreText);
-        FeverGaugeDown();
-    }
-
     public void GetScore()
     {
-        if (fever)
-        {
-            curScore += GameManager.SCORE * 2;
-        }
-        else
-        {
-            curScore += GameManager.SCORE;
-            FeverGaugeUp();
-        }
+        curScore += score;
+        FeverGaugeUp();
+        TextBuilder(score.ToString(), scoreText);
+        animator.SetTrigger("Get");
     }
 
     private void FeverGaugeUp()
     {
+        if (fever) return;
+
         if (feverGauge.value == feverGauge.maxValue)
         {
             fever = true;
+            score *= 2;
         }
         else
         {
@@ -74,8 +91,9 @@ public class InGameManager : MonoBehaviour
             if (feverGauge.value == feverGauge.minValue)
             {
                 fever = false;
+                score = GameManager.SCORE;
             }
-            else 
+            else
             {
                 feverGauge.value -= getFeverGauge * Time.deltaTime;
             }
